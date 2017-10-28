cae5d883ccbbbdbc07bd926008b2690d1b61747b
Cristian Oliveira <cristian.oliveiradarosa@barzahlen.de>
2017-10-28 11:38:51 +0200

    chore: remove api

--- api/config/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379/0' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379/0' }
-end
